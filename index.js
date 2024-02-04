3f5f26d0f1de62909cd2e5f97465370a471cc36f
Marwa Mohammed <52058731+MarwaElasuty@users.noreply.github.com>
2024-02-04 12:03:36 +0200

    fd

---
+++ index.js
@@ -0,0 +1,17 @@
+// var toggle = document.querySelector(".ul-list");
+//        function appear(){
+//         if(toggle.style.display=='none'){
+//           toggle.style.display='block';
+//         }
+//         else{
+//           toggle.style.display='none';
+//         }
+//        }
+
+
+       
+function appear() {
+         document.querySelector(".ul-list").classList.toggle("display");
+        }
+
+       
